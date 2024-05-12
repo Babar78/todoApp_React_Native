f126ddfd921977ff6c2f0a08526952356b2c72bc
Babar78 <muhammadbabarashraf8@gmail.com>
2024-05-12 05:48:45 +0500

    chore: Add delete functionality to Task component

--- components/Task.js
+++ components/Task.js
@@ -1,27 +1,73 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Modal from "react-native-modal";
+import { Button } from 'react-native-paper';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Task = (props) => {
     const [isChecked, setIsChecked] = useState(false);
+    const [isModalVisible, setModalVisible] = useState(false);
 
     const handleToggle = () => {
         setIsChecked(!isChecked);
     };
 
-    return (
-        <View style={styles.item}>
-            <View style={styles.itemLeft}>
-                <TouchableOpacity style={styles.checkbox} onPress={handleToggle}>
-                    {isChecked && <View style={styles.checkedIndicator} />}
-                </TouchableOpacity>
-                <Text style={[styles.itemText, isChecked && styles.itemTextChecked]}>
-                    {props.text}
-                </Text>
-            </View>
-            <View style={[styles.circular, isChecked && styles.completeCircular]}>
+    const toggleModal = () => {
+        setModalVisible(!isModalVisible);
+    }
 
-            </View>
-        </View>
+    const handleDelete = () => {
+        // Delete the task
+        props.completeTask(props.itemIndex);
+    }
+
+
+    return (
+        <>
+            <Modal isVisible={isModalVisible}>
+                <View style={{
+                    backgroundColor: 'white',
+                    padding: 22,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderRadius: 4,
+                    borderColor: 'rgba(0, 0, 0, 0.1)',
+                }}>
+                    <Text style={{
+                        fontSize: 18,
+                    }}>
+                        Are you sure you want to delete this task?
+                    </Text>
+                    <View style={{ width: "100%", flexDirection: 'row', justifyContent: "flex-end", marginTop: 20, gap: 10, }}>
+                        <Button mode="contained" onPress={toggleModal} style={{
+                            backgroundColor: 'gray',
+                            color: 'white',
+                        }}>Cancel</Button>
+                        <Button mode="contained" onPress={handleDelete} style={{
+                            backgroundColor: 'red',
+                            color: 'white',
+                        }}>
+                            Delete
+                        </Button>
+                    </View>
+                </View>
+            </Modal>
+
+            <View style={styles.item}>
+                <View style={styles.itemLeft}>
+                    <TouchableOpacity style={styles.checkbox} onPress={handleToggle}>
+                        {isChecked && <View style={styles.checkedIndicator} />}
+                    </TouchableOpacity>
+                    <Text style={[styles.itemText, isChecked && styles.itemTextChecked]}>
+                        {props.text}
+                    </Text>
+                </View>
+
+                {/* Add a button with Delete Icon */}
+                <Icon name="delete-circle" size={28} color="gray" onPress={toggleModal} />
+            </View >
+        </>
     );
 };
 
